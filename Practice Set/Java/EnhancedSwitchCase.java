67da144cf9a4c0d020ea3519c1fa7ca5dc008b0b
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2023-10-02 18:25:28 +0530

    Updated Java practice set

---
+++ Practice Set/Java/EnhancedSwitchCase.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EnhancedSwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of day : ");
+        int days = sc.nextInt();
+
+        switch (days){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Please enter a valid number between 1 - 7 ");
+        }
+
+    }
+}
