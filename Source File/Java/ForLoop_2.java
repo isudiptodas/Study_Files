8b7ae33de1731a5070fdb4bcbcb969456dcb5358
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2023-10-07 04:35:16 +0530

    Delete Source File/Java/ForLoop_2.java

--- Source File/Java/ForLoop_2.java
+++
@@ -1,9 +0,0 @@
-public class ForLoop2 {
-    public static void main(String[] args) {
-
-        for(int i = 1 ; i <= 10 ; i ++) {
-            System.out.println("2 x " +i + " = " +i*2);
-        }
-
-    }
-}
