10704ebbf4f449a381cc93d96a781e130f654dd0
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2023-11-19 23:29:29 +0530

    Added new JAVA practice set

---
+++ Practice Set/Java/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays{
+    public static void main(String args[]){
+
+        int Arr[] = {26, 24, 32, 47, 75, 83, 91, 12};
+
+        for(int i=0; i<=4; i++){
+            System.out.println("The element at index "+i +" is " +Arr[i]);
+        }
+    }
+
+}
