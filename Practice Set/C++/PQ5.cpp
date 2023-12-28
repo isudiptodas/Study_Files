88d5cdad611f56e02869f31ae436880a959082b7
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2023-12-29 00:05:22 +0530

    Add files via upload

---
+++ Practice Set/C++/PQ5.cpp
@@ -0,0 +1,13 @@
+// Practice Question 5 : guess the output
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   for(int i=0; i<=5; i++){
+       cout<<i<<" ";
+       i++;
+   }
+   
+}
---
+++ Practice Set/C++/PQ7.cpp
@@ -0,0 +1,17 @@
+// Practice Question 7 : guess the output
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   for(int i=0; i<=15; i+=2){
+       cout<<i<<" ";
+       if(i&1){
+           continue;
+       }
+       
+       i++;
+   }
+   
+}
