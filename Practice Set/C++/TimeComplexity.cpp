5066a3df6fc7e1d86e6000b0a36a076d1c4ac26e
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2024-01-02 15:46:33 +0530

    Add files via upload

---
+++ Practice Set/C++/TimeComplexity.cpp
@@ -0,0 +1,35 @@
+// Time complexity
+
+/*
+
+Conatant time     ->   O(1)
+Linear time       ->   O(n)
+Logarithmic time  ->   O(log n)
+Quadratic time    ->   O(n²)
+Cubic time        ->   O(n³)
+__________________________________________
+
+O(n!)
+O(2⁴)
+O(2³)
+O(2²)
+O(n log n)
+O(n)
+O(log n)
+O(1)
+
+The higher of the chart, the higher is the complexity
+________________________________________________________
+
+Some example questions : 
+
+2n² + 3n      - Ignore the least one(n), the time complexity is O(n²)
+4n⁴ + 3³      - Ignore the least one(n³), the time complexity is O(n⁴)
+n² + Log n    - Ignore the least one(n), the time complexity is O(n²)
+12001         - Since the number is constant the time complexity is O(1)
+3n³ + 2n² + 5 - Time complexity is O(n³)
+n³/300        - Ignore the number, the time complexity is O(n³)
+n/4           - Time complexity will be O(n)
+n + 4/4       - Ignore number again, time complexity will be O(n)
+
+*/
