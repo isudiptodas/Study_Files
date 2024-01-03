422dea67b10ef90fd29305c8fc2e55c5b7bcae50
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2024-01-03 17:19:27 +0530

    Add files via upload

---
+++ Practice Set/DSA in C++/PivotElement.cpp
@@ -0,0 +1,27 @@
+// Find pivot element
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int arr[] = {3, 8, 10, 17, 1};
+    int size = 5;
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end-start)/2;
+    
+    while(start<end){
+        if(arr[mid]>=arr[0]){
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+        
+        mid = start + (end-start)/2;
+    }
+    
+    cout<<"Pivot element is "<<arr[start];
+    
+}
