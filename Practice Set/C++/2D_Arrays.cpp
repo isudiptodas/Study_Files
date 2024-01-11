0114369285b264273606bd9ada8201c07504dbf5
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2024-01-11 23:57:08 +0530

    Update 2D_Arrays.cpp

--- Practice Set/C++/2D_Arrays.cpp
+++ Practice Set/C++/2D_Arrays.cpp
@@ -7,14 +7,14 @@ int main(){
     
     int arr[3][4];        // First box is row and second box is column
     
-    for(int row=0; row<3; i++){
-        for(int col=0; col<4; j++){
+    for(int row=0; row<3; row++){
+        for(int col=0; col<4; col++){
             cin>>arr[row][col];
         }
     }
     
-    for(int row=0; row<3; i++){
-        for(int col=0; col<4; j++){
+    for(int row=0; row<3; row++){
+        for(int col=0; col<4; col++){
             cout<<arr[row][col]<<" ";
         }
         cout<<endl;
